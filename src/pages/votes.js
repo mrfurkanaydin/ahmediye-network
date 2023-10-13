5652410ba29984533869d9ef5ca917cb9e3d0d47
Furkan <aydnfurkan007@gmail.com>
2023-10-13 12:10:30 +0300

    frontend updated

--- src/pages/votes.js
+++ src/pages/votes.js
@@ -48,7 +48,7 @@ export default function Votes({ allPosts, zort }) {
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col justify-center items-center gap-3 mx-5">
+      <div className="flex flex-col justify-center items-center gap-3 mx-2">
         <div className="text-2xl font-bold mt-3 my-2">Puan Sistemi</div>
         <>
           {datas.map((data) => {
@@ -60,7 +60,7 @@ export default function Votes({ allPosts, zort }) {
             );
             const averageVote = sumVotes / totalVotes;
             return (
-              <div key={data.name} className="flex flex-col gap-2 items-center">
+              <div key={data.name} className="flex flex-col gap-2 w-2/3 items-center justify-center border p-5 rounded">
                 <div className="text-2xl font-bold">{data.name}</div>
                 <div>
                   {averageVote > 5 ? (
@@ -78,7 +78,7 @@ export default function Votes({ allPosts, zort }) {
                   type="number"
                   min={1}
                   max={10}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="1 - 10 Arası Puan Veriniz"
                   required
                   onChange={(event) => {
@@ -91,7 +91,7 @@ export default function Votes({ allPosts, zort }) {
                 <button
                   onClick={() => submitForm(data.name)}
                   disabled={findItemByName(data.name)}
-                  className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                  className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >
                   {findItemByName(data.name) ? (
                     <>Puan Verdiniz</>
